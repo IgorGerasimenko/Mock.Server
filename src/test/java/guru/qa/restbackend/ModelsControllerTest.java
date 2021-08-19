ebe3fd58c25f20a538e16bb3512f1d649d5768f9
IgorGerasimenko <58310275+IgorGerasimenko@users.noreply.github.com>
2021-08-20 01:11:10 +0400

    Добавил Тесты

--- src/test/java/guru/qa/restbackend/ModelsControllerTest.java
+++ src/test/java/guru/qa/restbackend/ModelsControllerTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.with;
+import static org.hamcrest.Matchers.*;
 
 public class ModelsControllerTest {
 
@@ -18,22 +20,75 @@ public class ModelsControllerTest {
 
     private RequestSpecification spec =
             with()
-            .baseUri("http://localhost:8081")
+            .baseUri("http://localhost:8080")
             .basePath("/");
 
     @Test
-    void bankControllerTest() {
-        ModelInfo[] userInfos = spec.get("user/getAll")
+    void getModelsListTest() {
+        given()
+                .spec(spec)
+                .when()
+                .get("/models/getAll")
                 .then()
                 .statusCode(200)
-                .extract()
-                .response()
-                .as(ModelInfo[].class);
-
-        Stream.of(userInfos)
-                .filter(userInfo -> userInfo.getUserName().equals("Dima"))
-                .peek(userInfo -> System.out.println(userInfo.getUserName()))
-                .map(userInfo -> userInfo.toString())
-                .collect(Collectors.toList());
+                .body("", hasItems(hasEntry("modelPath", "app/models/Комната 2.fbx")));
     }
+
+    @Test
+    void addModelTest() {
+        given()
+                .spec(spec)
+                .contentType("application/json")
+                .body("{\n" +
+                        "\"modelPath\": \"app/models/Уфа модель.fbx\",\n" +
+                        "\"modelName\": \"Уфа модель\",\n" +
+                        "\"message\": null,\n" +
+                        "\"ownerId\": 2,\n" +
+                        "\"modelId\": null\n" +
+                        "}")
+                .when()
+                .post("models/add")
+                .then()
+                .statusCode(200)
+                .body("message", is("Модель успешно добавлена"));
+    }
+
+    @Test
+    void renameModelTest() {
+        String expResult = "Имя модели oldName успешно изменено на newName";
+//        String expResultWithoutQuotes = expResult.substring(1, expResult.length() - 1);
+        given()
+                .spec(spec)
+                .contentType("application/json")
+                .when()
+                .put("models/put/oldName/newName")
+                .then()
+                .statusCode(200)
+                .body(contains(expResult));
+    }
+
+    @Test
+    void deleteModelTest() {
+        given()
+                .spec(spec)
+                .contentType("application/json")
+                .when()
+                .delete("models/put/Name")
+                .then()
+                .statusCode(200)
+                .body(contains("Модель Name успешна удалена"));
+    }
+
+    @Test
+    void findModelTest() {
+        given()
+                .spec(spec)
+                .contentType("application/json")
+                .when()
+                .get("models/getByName/Уфа модель")
+                .then()
+                .statusCode(200)
+                .body("modelName", is("Уфа модель"));
+    }
+
 }
